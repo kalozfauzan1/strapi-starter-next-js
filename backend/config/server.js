d1a7ac4f09a92830e04e87182becb26d79966480
kaloz fauzan <kaloz.fauzan2@gmail.com>
2021-07-22 21:47:53 +0700

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2ab212322585daf22c1032b263b371f5'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '19fcae5d-fb88-42a0-b4a9-fe9af1e66d33'
+};
